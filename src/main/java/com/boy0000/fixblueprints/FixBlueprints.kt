e3f82b05e0e3b1e716f93524e9b588350ec0976b
Boy <sivertpaulsen2@gmail.com>
2022-09-18 13:24:23 +0200

    add logging and cmd

--- src/main/java/com/boy0000/fixblueprints/FixBlueprints.kt
+++ src/main/java/com/boy0000/fixblueprints/FixBlueprints.kt
@@ -1,11 +1,25 @@
 package com.boy0000.fixblueprints
 
+import org.bukkit.Bukkit
+import org.bukkit.command.Command
+import org.bukkit.command.CommandExecutor
+import org.bukkit.command.CommandSender
 import org.bukkit.plugin.java.JavaPlugin
 import java.io.File
 
+class FixBluePrintCommands : CommandExecutor {
+    override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>?): Boolean {
+        if (label == "fixblueprints")
+            fixBlueprints.fixBluePrints()
+        return true
+    }
+}
+
+val fixBlueprints: FixBlueprints by lazy { Bukkit.getPluginManager().getPlugin("FixBlueprints") as FixBlueprints }
 class FixBlueprints : JavaPlugin() {
     override fun onEnable() {
         // Plugin startup logic
+        getCommand("fixblueprints")?.setExecutor(FixBluePrintCommands())
         fixBluePrints()
     }
 
@@ -14,17 +28,26 @@ class FixBlueprints : JavaPlugin() {
     }
 
     private val blueprintDir = File(this.dataFolder.parent, "/ModelEngine/blueprints/")
-    private fun fixBluePrints() {
-        if (blueprintDir.exists())
+    private val prefix = "<red>[FixBlueprints] "
+    private fun log(msg: String) {
+        Bukkit.getConsoleSender().sendRichMessage(prefix + msg)
+    }
+
+    fun fixBluePrints() {
+        if (blueprintDir.exists()) {
+            log("<green>Scanning directories...")
             scanDirectory(blueprintDir)
+            log("<green>Finished fixing blueprints!")
+        }
     }
 
     // Recursive function to read all files down until it gets a BBModel file
     private fun scanDirectory(blueprintDir: File) {
         blueprintDir.listFiles()?.forEach blueprint@{ blueprintFile ->
             if (blueprintFile.name.endsWith(".bbmodel")) {
+                log("<green>Found BBModel file: <gold>${blueprintFile.name}")
                 // Quick fix all mob paths and npc paths
-                //blueprintFile.correctInitialTexturePaths()
+                blueprintFile.correctInitialTexturePaths()
                 blueprintFile.readBBModel()
             } else scanDirectory(blueprintFile)
         }
@@ -35,44 +58,44 @@ class FixBlueprints : JavaPlugin() {
         val paths = blueprint.split("{\"path\":\"").drop(1).map { it.substringBefore(",\"id\"") }
 
         paths.forEachIndexed { i, s ->
-            // Might reuse texture so make sure id is correct just in case
-            if (s.substringAfter("\"id\":\"").substringBefore("\"").toIntOrNull() != i) return@forEachIndexed
-
             val texturePath = s.substringBefore("\",\"name\"")
-            val newNamespace =
-                texturePath.substringAfter("\"namespace\":\"").substringBefore("\"").substringAfter("assets/")
-                    .substringBefore("/textures")
-            val newTexture = texturePath.substringAfter("\"name\":\"").substringBefore("\"").substringAfterLast("/")
-                .takeIf { it.endsWith(".png") } ?: return@forEachIndexed
-            val newPath = texturePath.substringAfter("\"folder\":\"").substringBefore("\"")
-                .substringAfter("assets/$newNamespace/textures/").substringBefore("/$newTexture")
-            val template =
-                "{\"path\",\"$texturePath\",\"name\":\"$newTexture\",\"folder\":\"$newPath\",\"namespace\":\"$newNamespace\""
+            val newNamespace = texturePath.substringAfter("assets/").substringBefore("/textures")
+            val newTexture = texturePath.substringAfterLast("/").takeIf { it.endsWith(".png") } ?: return@forEachIndexed
+            val newPath = texturePath.substringAfter("assets/$newNamespace/textures/").substringBefore("/$newTexture")
+            val template = "{\"path\":\"$texturePath\",\"name\":\"$newTexture\",\"folder\":\"$newPath\",\"namespace\":\"$newNamespace\""
+
             blueprint = blueprint
-                .replace(",\"relative_path\":\"$texturePath", ",\"relative_path\":\"$texturePath")
+                .replace(",\"relative_path\":\"$s", ",\"relative_path\":\"$texturePath")
                 .replace("{\"path\":\"$s", template)
                 .replace("\"saved\":false", "\"saved\":true")
         }
         writeText(blueprint)
+        log("<green>Fixed <italic>$name\n")
     }
 
     private fun File.correctInitialTexturePaths() {
         val blueprint = readText().replace("\\\\", "/")
         val parent = parent.replace("\\\\", "/")
-        val assets = "assets/minecraft/textures/"
+        val assets = "assets/minecraft/textures"
         when {
-            parent.endsWith("mobs") ->
+            parent.endsWith("mobs") -> {
+                log("<yellow>Correcting initial texture paths...")
                 writeText(blueprint.replace("$assets/creatures", "assets/mineinabyss/textures/mobs"))
+            }
+
             parent.endsWith("npcs") -> {
+                log("<yellow>Correcting initial texture paths...")
                 writeText(blueprint.replace("$assets/characters", "assets/mineinabyss/textures/characters"))
                 writeText(blueprint.replace("$assets/creatures", "assets/mineinabyss/textures/characters"))
             }
             parent.endsWith("relics") -> {
+                log("<yellow>Correcting initial texture paths...")
                 writeText(blueprint.replace("$assets/items", "assets/mineinabyss/textures/relics"))
                 writeText(blueprint.replace("$assets/relics", "assets/mineinabyss/textures/relics"))
 
             }
             parent.endsWith("blocks") -> {
+                log("<yellow>Correcting initial texture paths...")
                 writeText(blueprint.replace("$assets/block", "assets/mineinabyss/textures/blocks"))
                 writeText(blueprint.replace("$assets/block/modelengine", "assets/mineinabyss/textures/blocks"))
             }
